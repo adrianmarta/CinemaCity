231fc6c13825a564318d9500a46026d814e3acee
adrianmarta <adymarta03@gmail.com>
2024-03-11 16:27:41 +0200

    first

---
+++ CinemaCity/src/test/java/com/CinemaCity/CinemaCity/CinemaCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CinemaCity.CinemaCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
