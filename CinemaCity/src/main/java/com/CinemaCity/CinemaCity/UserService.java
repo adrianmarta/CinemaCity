9eda481f68a2ee8d9cb4626f0458342919957cd4
Christi <christianalexandrudragoi@gmail.com>
2024-04-18 11:47:59 +0300

    better joinParty method but still doesnt work well, and set joined participants list when created new party

--- CinemaCity/src/main/java/com/CinemaCity/CinemaCity/UserService.java
+++ CinemaCity/src/main/java/com/CinemaCity/CinemaCity/UserService.java
@@ -2,7 +2,10 @@ package com.CinemaCity.CinemaCity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,7 +25,11 @@ public class UserService {
     public Optional<User> singleUserByEmail(String email) {
         return userRepository.findUserByEmail(email);
     }
+    @Transactional
     public void joinParty(Party party,User user){
-    party.getJoined_participants().add(user);
+        if(party == null) throw new IllegalArgumentException("Party doesn't exist");
+        if(party.getJoined_participants() != null && party.getJoined_participants().contains(user))
+            throw new IllegalStateException("User already joined");
+        party.getJoined_participants().add(user);
     }
 }
