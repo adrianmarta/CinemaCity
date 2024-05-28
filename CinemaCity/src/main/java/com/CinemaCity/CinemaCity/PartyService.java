118e010df0a8ad37edf22155d5fe3a4a58bf9d0c
Christi <christianalexandrudragoi@gmail.com>
2024-05-28 14:51:48 +0300

    bug

--- CinemaCity/src/main/java/com/CinemaCity/CinemaCity/PartyService.java
+++ CinemaCity/src/main/java/com/CinemaCity/CinemaCity/PartyService.java
@@ -60,7 +60,7 @@ public class PartyService {
         if (party.getJoined_participants() != null && party.getJoined_participants().contains(user)) {
             throw new IllegalStateException("User already joined");
         }
-        if(party.getJoined_participants().size()<=party.getMax_participants()){
+        if(party.getJoined_participants().size()>=party.getMax_participants()){
             throw new IllegalStateException("You can't join this party, it's full!");
         }
         if (party.getHostUser().equals(user)) {
